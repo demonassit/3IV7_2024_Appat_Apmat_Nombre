344479bcad91c29fbe721ed2d741272c7ec4b93e
demonassit <jminorg@ipn.mx>
2024-10-21 17:35:02 -0600

    Tarea convertir de binario a decimal y decimal a binario

---
+++ Python/01Ejemplo/holaMundo.py
@@ -0,0 +1,33 @@
+#esto es un comentario
+
+#vamos a definir una variable
+
+#ejemplo de if una condicion
+
+#x = 28
+
+#if x < 0:
+    #cuadno sea verdad
+   # print('Es menor que 0')
+#elif x > 0 :
+   #print('Es mayor que 0')
+#else :
+   # print('Es 0')
+
+
+#bucles
+#numero = 0
+#print("Tabla del 2")
+
+#while numero <= 10 :
+    #print("Resultado :" , 2*numero)
+    #print("Antes", numero)
+    #numero+=1
+    #print("Despues: ", numero)
+
+#bucle for
+
+numero = [3, 7, 5, 8]
+
+for n in numero :
+    print(n)
---
+++ Python/02Entrada/calculadora.py
@@ -0,0 +1,11 @@
+#vamos a crear una calculadora sencilla
+#para la entrada de datos es necesario utilizar el metodo
+
+numero1 = float(input("Introduce el primer número: "))
+numero2 = float(input("Introduce el segundo número: "))
+
+#vamos a sumar
+
+suma = numero1 + numero2
+
+print("La suma es de: ", suma)
