5936f8c87f14e7c50c61c7c509fc18ff37710fc5
demonassit <jminorg@ipn.mx>
2024-11-05 18:55:20 -0600

    metodos de archivos leer y escribir

--- Python/05Listas/crud.py
+++ Python/05Listas/crud.py
@@ -4,10 +4,52 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog
 
+#para poder guardar los datos correspondientes de la lista es necesario utilizar un archivo, para ello vamos a ocupar la libreria os
+import os
+
+#vamos a declarar un archivo, tenemos dos opciones una ruta dinamica o una ruta estatica, eso queda de tarea 
+ARCHIVO = "alumnos.txt"
+
 #el examen debe de tener almenos 8 elementos de la lista que deseen guardar el examen debe de posee elementos de dialogo y mensajes de salida con tkinter, la lista debe de implementar al menos 30 diferentes elementos y debe verse una interfaz mediante la cual los imprima en formato de lista
 
 #primero vamos a crear una lista de alumnos
 alumnos = []
+
+#vamos a crear una funcion para cargar datos
+def cargar_datos():
+    #verificar si existe el archivo
+    if os.path.exists(ARCHIVO):
+        with open(ARCHIVO, "r") as f:
+            for linea in f:
+                #que voy a obtener por cada linea
+                #es un metodo de cadena que nos ayuda a eliminar espacios al inicio y final de una cadena " habia     "
+                partes = linea.strip().split(",")
+                if len(partes) >= 6:
+                    boleta = partes[0]
+                    nombre = partes[1]
+                    apellido_paterno = partes[2]
+                    apellido_materno = partes[3]
+                    fecha_nacimiento = partes[4]
+                    calificaciones  = list(map(float, partes[5:]))
+                    #defino al alumno
+                    alumno = {
+                        "boleta" : boleta,
+                        "nombre" : nombre,
+                        "apellido_paterno" : appat,
+                        "apellido_materno" : apmat,
+                        "fecha_nacimiento" : fecnac,
+                        "calificaciones" : calificaciones
+                    }
+                    alumnos.append(alumno)
+
+#vamos a crear la funcion para guardar los datos
+def guardar_datos():
+    with open(ARCHIVO, "w") as f:
+        for alumno in alumnos:
+            f.write(f"{alumno['boleta']},{alumno['nombre']},{alumno['apellido_paterno']},{alumno['apellido_materno']},{alumno['fecha_nacimiento']}, {','.join(map(str,alumno['calificaciones']))}\n")
+
+
+
 
 #vamos a crear una funcion que se encargue de registrar un alumno
 
@@ -35,6 +77,8 @@ def registrar_alumno():
     }
 
     alumnos.append(alumno)
+    #aqui tengo que mandar a llamar a 
+    guardar_datos()
     messagebox.showinfo("Exito", "Alumno registrado exitosamente")
 
 #funcion para poder consultar los datos del arreglo de alumnos(lista)
@@ -44,7 +88,7 @@ def consultar_alumnos() :
     else :
         print("Lista de Alumnos: \n")
         for alumno in alumnos :
-            print("Boleta: {alumno['boleta']}, Nombre: {alumno['nombre']}{alumno['apellido_paterno']}{alumno['apellido_materno']}, Fecha de Nacimiento: {alumno['fecha_nacimiento']}, Calificaciones: {alumno['calificaciones']} \n" )
+            print(f"Boleta: {alumno['boleta']}, Nombre: {alumno['nombre']}{alumno['apellido_paterno']}{alumno['apellido_materno']}, Fecha de Nacimiento: {alumno['fecha_nacimiento']}, Calificaciones: {alumno['calificaciones']} \n" )
 
 #funcion para buscar y editar por la boleta
 def editar_alumno() :
